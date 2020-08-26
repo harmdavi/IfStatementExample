8eb26b810d93df7e071306fa2305c4dd25ba3667
harmdavi <harmondrum@gmail.com>
2020-08-26 08:52:40 -0600

    Add project files.

---
+++ IfStatements.vb
@@ -0,0 +1,35 @@
+﻿Option Strict On
+Option Explicit On
+Option Compare Binary
+
+Module IfStatements
+
+    Sub Main()
+        Dim userInput As String
+        Dim userNumber As Integer
+        Const COMPAREWITH As Integer = 5
+        'If 3 > 5 Then
+        '    Console.WriteLine("Yep")
+        'Else
+        '    Console.WriteLine("Nope")
+        'End If
+
+        userInput = Console.ReadLine()
+        userNumber = CInt(userInput)
+
+        If userNumber > 5 Then
+            Console.WriteLine("Biggger than 5")
+        ElseIf userNumber < 5 Then
+            Console.WriteLine("Smaller than 5")
+        ElseIf userNumber = 5 Then
+            Console.WriteLine("Equals 5")
+        Else
+            Console.WriteLine("Invalid Input")
+        End If
+
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("IfStatementExample")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("IfStatementExample")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("62943b23-876d-4bda-a5c6-df8de4786698")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
